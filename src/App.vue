39bad74043b7ff82ed1d4243800445e1afb7e9f1
marvinaic <302817759@student.rocmondriaan.nl>
2021-06-08 15:12:54 +0200

    commit1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="raster">
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import Snake from './components/Snake.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //Snake
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/Snake.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Snake',
+  data() {
+    return{
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
